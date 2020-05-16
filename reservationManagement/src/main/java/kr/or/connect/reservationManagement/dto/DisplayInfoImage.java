52f60c05cb010a022a4aaa287b7320f44cf8c2aa
ChoiKanghun <rkdgns4562@naver.com>
2020-05-16 17:26:28 +0900

    Create: created DisplayInfoImage DTO

---
+++ reservationManagement/src/main/java/kr/or/connect/reservationManagement/dto/DisplayInfoImage.java
@@ -0,0 +1,78 @@
+package kr.or.connect.reservationManagement.dto;
+
+public class DisplayInfoImage {
+	private String contentType;
+	private String createDate;
+	private String modifyDate;
+	private boolean deleteFlag;
+	private String fileName;
+	private int fileId;
+	private int displayInfoId;
+	private int displayInfoImageId;
+	private String saveFileName;
+	
+	public String getContentType() {
+		return contentType;
+	}
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getModifyDate() {
+		return modifyDate;
+	}
+	public void setModifyDate(String modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+	public boolean isDeleteFlag() {
+		return deleteFlag;
+	}
+	public void setDeleteFlag(boolean deleteFlag) {
+		this.deleteFlag = deleteFlag;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public int getFileId() {
+		return fileId;
+	}
+	public void setFileId(int fileId) {
+		this.fileId = fileId;
+	}
+	public int getDisplayInfoId() {
+		return displayInfoId;
+	}
+	public void setDisplayInfoId(int displayInfoId) {
+		this.displayInfoId = displayInfoId;
+	}
+	public int getDisplayInfoImageId() {
+		return displayInfoImageId;
+	}
+	public void setDisplayInfoImageId(int displayInfoImageId) {
+		this.displayInfoImageId = displayInfoImageId;
+	}
+	public String getSaveFileName() {
+		return saveFileName;
+	}
+	public void setSaveFileName(String saveFileName) {
+		this.saveFileName = saveFileName;
+	}
+	@Override
+	public String toString() {
+		return "DisplayInfoImage [contentType=" + contentType + ", createDate=" + createDate + ", modifyDate="
+				+ modifyDate + ", deleteFlag=" + deleteFlag + ", fileName=" + fileName + ", fileId=" + fileId
+				+ ", displayInfoId=" + displayInfoId + ", displayInfoImageId=" + displayInfoImageId + ", saveFileName="
+				+ saveFileName + "]";
+	}
+	
+	
+	
+}
