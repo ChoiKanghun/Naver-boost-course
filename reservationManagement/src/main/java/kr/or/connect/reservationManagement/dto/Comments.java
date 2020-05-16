1a890ae4511ad598236c865ca1eba8cb0b201ae3
ChoiKanghun <rkdgns4562@naver.com>
2020-05-16 17:25:23 +0900

    Create: comments dto

---
+++ reservationManagement/src/main/java/kr/or/connect/reservationManagement/dto/Comments.java
@@ -0,0 +1,100 @@
+package kr.or.connect.reservationManagement.dto;
+
+import java.util.List;
+
+public class Comments {
+	private List<CommentImages> commentImages;
+	private String comment;
+	private int commentId;
+	private String createDate;
+	private String modifyDate;
+	private int productId;
+	private String reservationDate;
+	private String reservationEmail;
+	private int reservationInfoId;
+	private String reservationName;
+	private String reservationTelephone;
+	private Float score;
+	public List<CommentImages> getCommentImages() {
+		return commentImages;
+	}
+	public void setCommentImages(List<CommentImages> commentImages) {
+		this.commentImages = commentImages;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public int getCommentId() {
+		return commentId;
+	}
+	public void setCommentId(int commentId) {
+		this.commentId = commentId;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getModifyDate() {
+		return modifyDate;
+	}
+	public void setModifyDate(String modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getReservationDate() {
+		return reservationDate;
+	}
+	public void setReservationDate(String reservationDate) {
+		this.reservationDate = reservationDate;
+	}
+	public String getReservationEmail() {
+		return reservationEmail;
+	}
+	public void setReservationEmail(String reservationEmail) {
+		this.reservationEmail = reservationEmail;
+	}
+	public int getReservationInfoId() {
+		return reservationInfoId;
+	}
+	public void setReservationInfoId(int reservationInfoId) {
+		this.reservationInfoId = reservationInfoId;
+	}
+	public String getReservationName() {
+		return reservationName;
+	}
+	public void setReservationName(String reservationName) {
+		this.reservationName = reservationName;
+	}
+	public String getReservationTelephone() {
+		return reservationTelephone;
+	}
+	public void setReservationTelephone(String reservationTelephone) {
+		this.reservationTelephone = reservationTelephone;
+	}
+	public Float getScore() {
+		return score;
+	}
+	public void setScore(Float score) {
+		this.score = score;
+	}
+	@Override
+	public String toString() {
+		return "Comments [commentImages=" + commentImages + ", comment=" + comment + ", commentId=" + commentId
+				+ ", createDate=" + createDate + ", modifyDate=" + modifyDate + ", productId=" + productId
+				+ ", reservationDate=" + reservationDate + ", reservationEmail=" + reservationEmail
+				+ ", reservationInfoId=" + reservationInfoId + ", reservationName=" + reservationName
+				+ ", reservationTelephone=" + reservationTelephone + ", score=" + score + "]";
+	}
+	
+	
+}
