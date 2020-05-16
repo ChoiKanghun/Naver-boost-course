ae621791dd2f3cc94b3bcdb297e2083d721c00a6
ChoiKanghun <rkdgns4562@naver.com>
2020-05-16 17:31:06 +0900

    Add: added methods which are related to following things
    DisplayInfo, DisplayInfoImage, Comments, CommentImages

--- reservationManagement/src/main/java/kr/or/connect/reservationManagement/service/ReservationManagementService.java
+++ reservationManagement/src/main/java/kr/or/connect/reservationManagement/service/ReservationManagementService.java
@@ -2,8 +2,14 @@ package kr.or.connect.reservationManagement.service;
 
 import java.util.List;
 
+import kr.or.connect.reservationManagement.dto.CommentImages;
+import kr.or.connect.reservationManagement.dto.Comments;
 import kr.or.connect.reservationManagement.dto.DetailPageItems;
+import kr.or.connect.reservationManagement.dto.DisplayInfo;
+import kr.or.connect.reservationManagement.dto.DisplayInfoImage;
 import kr.or.connect.reservationManagement.dto.Items;
+import kr.or.connect.reservationManagement.dto.ProductImages;
+import kr.or.connect.reservationManagement.dto.ProductPrices;
 import kr.or.connect.reservationManagement.dto.ReservationComments;
 
 public interface ReservationManagementService {
@@ -20,4 +26,10 @@ public interface ReservationManagementService {
 	List<DetailPageItems> getDetailListItems(Integer id);
 	List<ReservationComments> getUserCommentsById(Integer id);
 	List<ReservationComments> getLimitedUserCommentsById(Integer id, Integer limit);
+	List<CommentImages> getCommentImages(Integer reservationUserCommentId);
+	List<Comments> getComments(Integer displayInfoId);
+	List<ProductImages> getProductImages(Integer displayInfoId);
+	List<ProductPrices> getProductPrices(Integer displayInfoId);
+	DisplayInfoImage getDisplayInfoImage(Integer displayInfoId);
+	DisplayInfo getDisplayInfo(Integer displayInfoId);
 }
--- reservationManagement/src/main/java/kr/or/connect/reservationManagement/service/impl/ReservationManagementServiceImpl.java
+++ reservationManagement/src/main/java/kr/or/connect/reservationManagement/service/impl/ReservationManagementServiceImpl.java
@@ -5,11 +5,23 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kr.or.connect.reservationManagement.dao.CommentImagesDao;
+import kr.or.connect.reservationManagement.dao.CommentsDao;
 import kr.or.connect.reservationManagement.dao.DetailPageItemsDao;
+import kr.or.connect.reservationManagement.dao.DisplayInfoDao;
+import kr.or.connect.reservationManagement.dao.DisplayInfoImageDao;
 import kr.or.connect.reservationManagement.dao.ItemsDao;
+import kr.or.connect.reservationManagement.dao.ProductImagesDao;
+import kr.or.connect.reservationManagement.dao.ProductPricesDao;
 import kr.or.connect.reservationManagement.dao.ReservationCommentsDao;
+import kr.or.connect.reservationManagement.dto.CommentImages;
+import kr.or.connect.reservationManagement.dto.Comments;
 import kr.or.connect.reservationManagement.dto.DetailPageItems;
+import kr.or.connect.reservationManagement.dto.DisplayInfo;
+import kr.or.connect.reservationManagement.dto.DisplayInfoImage;
 import kr.or.connect.reservationManagement.dto.Items;
+import kr.or.connect.reservationManagement.dto.ProductImages;
+import kr.or.connect.reservationManagement.dto.ProductPrices;
 import kr.or.connect.reservationManagement.dto.ReservationComments;
 import kr.or.connect.reservationManagement.service.ReservationManagementService;
 
@@ -18,12 +30,22 @@ public class ReservationManagementServiceImpl implements ReservationManagementSe
 
 	@Autowired
 	ItemsDao productsDao;
-	
 	@Autowired
 	DetailPageItemsDao detailPageItemsDao;
-	
 	@Autowired
 	ReservationCommentsDao reservationCommentsDao;
+	@Autowired
+	CommentImagesDao commentImagesDao;
+	@Autowired
+	CommentsDao commentsDao;	
+	@Autowired
+	DisplayInfoDao displayInfoDao;
+	@Autowired
+	DisplayInfoImageDao displayInfoImageDao;
+	@Autowired
+	ProductImagesDao productImagesDao;
+	@Autowired
+	ProductPricesDao productPricesDao;
 	
 	@Override
 	public List<Items> getAllProducts(){
@@ -73,5 +95,35 @@ public class ReservationManagementServiceImpl implements ReservationManagementSe
 	@Override
 	public List<ReservationComments> getLimitedUserCommentsById(Integer id, Integer limit){
 		return reservationCommentsDao.getLimitedUserCommentById(id, limit);
+	}
+	
+	@Override
+	public List<CommentImages> getCommentImages(Integer reservationUserCommentId){
+		return commentImagesDao.getCommentImages(reservationUserCommentId);
+	}
+	
+	@Override
+	public List<Comments> getComments(Integer displayInfoId){
+		return commentsDao.getComments(displayInfoId);
+	}
+	
+	@Override
+	public List<ProductImages> getProductImages(Integer displayInfoId){
+		return productImagesDao.getProductImages(displayInfoId);
+	}
+	
+	@Override
+	public List<ProductPrices> getProductPrices(Integer displayInfoId){
+		return productPricesDao.getProductPrices(displayInfoId);
+	}
+	
+	@Override
+	public DisplayInfoImage getDisplayInfoImage(Integer displayInfoId) {
+		return displayInfoImageDao.getDisplayInfoImage(displayInfoId);
+	}
+	
+	@Override
+	public DisplayInfo getDisplayInfo(Integer displayInfoId) {
+		return displayInfoDao.getDisplayInfo(displayInfoId);
 	}
 }
