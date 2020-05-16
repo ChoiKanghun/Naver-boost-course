5d8cbc0b77178efed5095d622feca3ff8fa4f68c
ChoiKanghun <rkdgns4562@naver.com>
2020-05-16 17:31:20 +0900

    Create: created ProductPricesDao

---
+++ reservationManagement/src/main/java/kr/or/connect/reservationManagement/dao/ProductPricesDao.java
@@ -0,0 +1,34 @@
+package kr.or.connect.reservationManagement.dao;
+
+import static kr.or.connect.reservationManagement.dao.Sqls.SELECT_PRODUCT_PRICES_BY_DISPLAY_INFO_ID;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import kr.or.connect.reservationManagement.dto.ProductPrices;
+
+@Repository
+public class ProductPricesDao {
+	private NamedParameterJdbcTemplate jdbc;
+	private RowMapper<ProductPrices> rowMapper
+		= BeanPropertyRowMapper.newInstance(ProductPrices.class);
+
+	public ProductPricesDao(DataSource dataSource) {
+		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+	}
+	
+	public List<ProductPrices> getProductPrices(Integer displayInfoId) {
+		Map<String, Integer> params = new HashMap<>();
+		
+		params.put("displayInfoId", displayInfoId);
+		return jdbc.query(SELECT_PRODUCT_PRICES_BY_DISPLAY_INFO_ID, params, rowMapper);
+	}
+}
