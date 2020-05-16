12d4c61570e46df453c9588e3faa6a5a80cdce4f
ChoiKanghun <rkdgns4562@naver.com>
2020-05-16 17:31:46 +0900

    Create: created ProductImages DTO

---
+++ reservationManagement/src/main/java/kr/or/connect/reservationManagement/dto/ProductImages.java
@@ -0,0 +1,85 @@
+package kr.or.connect.reservationManagement.dto;
+
+public class ProductImages {
+	private String contentType;
+	private String fileName;
+	private String createDate;
+	private String modifyDate;
+	private boolean deleteFlag;
+	private int fileInfoId;
+	private int productId;
+	private int productImageId;
+	private String saveFileName;
+	private String type;
+	
+	public String getContentType() {
+		return contentType;
+	}
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getModifyDate() {
+		return modifyDate;
+	}
+	public void setModifyDate(String modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+	public boolean isDeleteFlag() {
+		return deleteFlag;
+	}
+	public void setDeleteFlag(boolean deleteFlag) {
+		this.deleteFlag = deleteFlag;
+	}
+	public int getFileInfoId() {
+		return fileInfoId;
+	}
+	public void setFileInfoId(int fileInfoId) {
+		this.fileInfoId = fileInfoId;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public int getProductImageId() {
+		return productImageId;
+	}
+	public void setProductImageId(int productImageId) {
+		this.productImageId = productImageId;
+	}
+	public String getSaveFileName() {
+		return saveFileName;
+	}
+	public void setSaveFileName(String saveFileName) {
+		this.saveFileName = saveFileName;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	@Override
+	public String toString() {
+		return "ProductImages [contentType=" + contentType + ", fileName=" + fileName + ", createDate=" + createDate
+				+ ", modifyDate=" + modifyDate + ", deleteFlag=" + deleteFlag + ", fileInfoId=" + fileInfoId
+				+ ", productId=" + productId + ", productImageId=" + productImageId + ", saveFileName=" + saveFileName
+				+ ", type=" + type + "]";
+	}
+	
+	
+	
+}
