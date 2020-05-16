70c0aae666c3eae82d066264da19bc6f57e99f8c
ChoiKanghun <rkdgns4562@naver.com>
2020-05-16 17:31:56 +0900

    Create: created ProductImagesDao

---
+++ reservationManagement/src/main/java/kr/or/connect/reservationManagement/dao/ProductImagesDao.java
@@ -0,0 +1,33 @@
+package kr.or.connect.reservationManagement.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import static kr.or.connect.reservationManagement.dao.Sqls.*;
+
+import kr.or.connect.reservationManagement.dto.ProductImages;
+
+@Repository
+public class ProductImagesDao {
+	private NamedParameterJdbcTemplate jdbc;
+	private RowMapper<ProductImages> rowMapper
+		= BeanPropertyRowMapper.newInstance(ProductImages.class);
+
+	public ProductImagesDao(DataSource dataSource) {
+		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+	}
+	
+	public List<ProductImages> getProductImages(Integer displayInfoId) {
+		Map<String, Integer> params = new HashMap<>();
+		
+		params.put("displayInfoId", displayInfoId);
+		return jdbc.query(SELECT_PRODUCT_IMAGES_BY_DISPLAY_INFO_ID, params, rowMapper);
+	}
+}
