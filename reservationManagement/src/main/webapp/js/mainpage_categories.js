c42d8947d0e9f6c1d04d293b24cf4f83441b90fb
ChoiKanghun <rkdgns4562@naver.com>
2020-06-08 20:49:12 +0900

    Save: save point 20200608

--- reservationManagement/src/main/webapp/js/mainpage_categories.js
+++ reservationManagement/src/main/webapp/js/mainpage_categories.js
@@ -1,122 +1,99 @@
-var test = document.querySelector("#test");
+document.addEventListener("DOMContentLoaded", () => {
+  var test = document.querySelector("#test");
+  var category_tab = document.querySelector(".event_tab_lst");
+  var start = 0;
+  var moreBtn = document.querySelector(".more");
+  var current_category = document.querySelector("#current_category").value;
 
-var category_tab = document.querySelector(".event_tab_lst");
-var start = 0;
-var moreBtn = document.querySelector(".more");
-var current_category = document.querySelector("#current_category").value;
-
-function drawBasicCategory(json){
+  function drawBasicCategory(json) {
+	var columns = document.querySelectorAll(".lst_event_box");
+	var leftColumn = columns[0];
+	var rightColumn = columns[1];
 	var itemListHTML = document.querySelector("#itemList").innerHTML;
+    var itemListResult = "";
 
-	var itemListResult = "<ul class='lst_event_box'>";
-
-	//replace all brackets in itemList template.innerHTML
-	//json.items.length would be 1~4
-	for(var i = 0; i < json.items.length; i++){
-		itemListResult += itemListHTML.replace("{display_info_id}", json.items[i].displayInfoId)
-							.replace("{src}", json.items[i].productImageUrl)
-							.replace("{description}", json.items[i].productDescription)
-							.replace("{placeName}", json.items[i].placeName)
-							.replace("{description}", json.items[i].productDescription)
-							.replace("{content}", json.items[i].productContent);
-		itemListResult += "</ul>";
-		document.querySelector(".more").insertAdjacentHTML("beforebegin", itemListResult);
-		itemListResult = "<ul class='lst_event_box'>";
-		//start count will be used to delete 'more' button from browser
-		start++;
-	}
-	
-	
-	if (start == json.totalCount)
-		document.querySelector(".more").style.display="none";
-}
+    for (var i = 0; i < json.items.length; i++) {
+      itemListResult = itemListHTML.replace("{display_info_id}", json.items[i].displayInfoId)
+        .replace("{src}", json.items[i].productImageUrl)
+        .replace("{description}", json.items[i].productDescription)
+        .replace("{placeName}", json.items[i].placeName)
+        .replace("{description}", json.items[i].productDescription)
+        .replace("{content}", json.items[i].productContent);
+      
+      if (i % 2 == 0)
+    	  rightColumn.innerHTML += itemListResult;
+      else
+    	  leftColumn.innerHTML += itemListResult;
+      start++;
+    }
+    if (start == json.totalCount)
+      document.querySelector(".more").style.display = "none";
+  }
 
 
-function makeHtmlContent(url, oReq, category_id){
-	//get json text
-	var json = JSON.parse(oReq.responseText);
-	var lst_event_boxes = document.querySelectorAll(".lst_event_box");
-	var event_tab_lst_li = document.querySelector(".event_tab_lst").querySelectorAll("li");
-	var pink = document.querySelector(".event_lst_txt").querySelector(".pink");
+  function makeHtmlContent(url, oReq, category_id) {
+    var json = JSON.parse(oReq.responseText);
+    var columns = document.querySelectorAll(".lst_event_box");
+    var event_tab_lst_li = document.querySelector(".event_tab_lst").querySelectorAll("li");
+    var pink = document.querySelector(".event_lst_txt").querySelector(".pink");
 
-	//remove all the content in .lst_event_box
-	lst_event_boxes.forEach(function(content){content.remove();	});
-	
-	//change category anchor 
-	event_tab_lst_li.forEach(function(list){
-		list.firstElementChild.className = 
-			(list.dataset.category == category_id) ? 'anchor active' : 'anchor'; 
-	});
-	
-	pink.innerText = json.totalCount + "개";
-	
-	//draw basic contents of categories 
-	drawBasicCategory(json);
-	
-}
+    columns.forEach(function(content) {
+      content.querySelectorAll("li").forEach(function(li){
+    	  li.remove();
+      })
+    });
+    event_tab_lst_li.forEach(function(list) {
+      list.firstElementChild.className =
+        (list.dataset.category == category_id) ? 'anchor active' : 'anchor';
+    });
+    pink.innerText = json.totalCount + "개";
+    drawBasicCategory(json);
+  }
+
+  function sendProductsAjax(url, flag, category_id) {
+    var oReq = new XMLHttpRequest;
+
+    oReq.open('GET', url);
+    oReq.setRequestHeader("Content-type", "application/json");
+    oReq.responseType = "text";
+    oReq.addEventListener("load", function() {
+      if (flag === "tab")
+        makeHtmlContent(url, this, category_id);
+      if (flag === "more")
+        drawBasicCategory(JSON.parse(oReq.responseText));
+    });
+    oReq.send();
+
+  }
+
+  category_tab.addEventListener("click", function(evt) {
+    var category_id = "";
+
+    if (evt.target.tagName === "UL") {
+      category_id = evt.target.firstChild.dataset.category;
+    } else if (evt.target.tagName === "LI") {
+      category_id = evt.target.dataset.category;
+    } else if (evt.target.tagName === "A") {
+      category_id = evt.target.parentElement.dataset.category;
+    } else if (evt.target.tagName === "SPAN") {
+      category_id = evt.target.parentElement.parentElement.dataset.category;
+    }
+    current_category = category_id;
+    start = 0;
+    sendProductsAjax("/reservationManagement/api/products?categoryId=" + category_id, "tab", category_id);
+    document.querySelector(".more").style.display = "block";
+  });
 
 
-	//send Ajax to get JSON info
-function sendProductsAjax(url, flag, category_id){
-	var oReq = new XMLHttpRequest;
-	
-	oReq.open('GET', url);
-	oReq.setRequestHeader("Content-type", "application/json");
-	oReq.responseType = "text";
-	oReq.addEventListener("load", function(){
 
-		if (flag === "tab")
-			makeHtmlContent(url, this, category_id);
-		if (flag === "more")
-			drawBasicCategory(JSON.parse(oReq.responseText));
-	});
-	oReq.send();
+  sendProductsAjax("/reservationManagement/api/products?categoryId=0", "tab", 0);
 
-}
+/*-----------------more button-----------*/
+  moreBtn.addEventListener("click", function(evt) {
+    var category_id = current_category;
 
-category_tab.addEventListener("click", function(evt){
-	var category_id = "";
+    sendProductsAjax("/reservationManagement/api/products?start=" + start + "&categoryId=" + category_id, "more", category_id);
+  })
 
-	//get category_id by event bubbling, delegation
-	if (evt.target.tagName === "UL"){
-		category_id = evt.target.firstChild.dataset.category;
-	}else if(evt.target.tagName === "LI"){
-		category_id = evt.target.dataset.category;
-	}else if(evt.target.tagName === "A"){
-		category_id = evt.target.parentElement.dataset.category;
-	}else if(evt.target.tagName === "SPAN"){
-		category_id = evt.target.parentElement.parentElement.dataset.category;
-	}
-	
-	//current_category is global variable;
-	current_category = category_id;
-	
-	//sendAjax to change the tab;
-	start = 0;
-	sendProductsAjax("/reservationManagement/api/products?categoryId=" + category_id, "tab", category_id);
 
-	
-	//in case more button's style.display is already none
-	document.querySelector(".more").style.display="block";
 });
-
-
-
-	//when the page loads
-document.addEventListener("DOMContentLoaded", function(){
-	//browser draws main List to ".event_tab_lst"
-	sendProductsAjax("/reservationManagement/api/products?categoryId=0", "tab", 0);
-});
-
-
-/* ------------------ more btn ----------------------- */
-
-moreBtn.addEventListener("click", function(evt){
-	var category_id = current_category;
-
-	
-	//sendAjax to get more category contents
-	sendProductsAjax("/reservationManagement/api/products?start=" + start + "&categoryId=" + category_id, "more", category_id);
-})
-
-
--- reservationManagement/src/main/webapp/js/mainpage_visual_image.js
+++ reservationManagement/src/main/webapp/js/mainpage_visual_image.js
@@ -1,71 +1,58 @@
-const PROMOTION_WIDTH = 414;
+document.addEventListener("DOMContentLoaded", () => {
+  const PROMOTION_WIDTH = 414;
+  
+  function promotion_image_move(image_count) {
+    var curr = 0;
+    var visual_img = document.querySelector(".visual_img");
 
-function promotion_image_move(image_count){
-	var curr = 0;
-	var visual_img = document.querySelector(".visual_img");
+    function set_t_out() {
+      setTimeout(function() {
+        if (curr != 0)
+          visual_img.style.transition = "transform 2s ease-in-out";
+        else
+          visual_img.style.transition = "";
+        visual_img.style.transform = "translate3d(-" + (curr) * PROMOTION_WIDTH + "px, 0px, 0px)";
 
-	//recursively executes setTimeout Method to make effect
-	function set_t_out(){
-		setTimeout(function(){
-		if (curr != 0){
-			visual_img.style.transition = "transform 2s ease-in-out";
-		}
-		else{
-			visual_img.style.transition = "";
-		}
-		visual_img.style.transform = "translate3d(-" + (curr) * PROMOTION_WIDTH + "px, 0px, 0px)";
+        curr++;
+        if (curr === image_count) {
+          curr = 0;
+        }
+        set_t_out();
+      }, 2000);
+    }
+    set_t_out();
+  }
 
-		//when the page get the last promotion image, 
-		//it goes back to the first one
-		curr++;
-		if( curr === image_count) {
-			curr = 0;
-		}
-		set_t_out();
-	}, 2000);
-}
-	//executes the function
-	set_t_out();
+  function addThImages(oReq) {
+    var json = JSON.parse(oReq.responseText);
+    var promotionItemHTML = document.querySelector("#promotionImages").innerHTML;
+    var promotionItemResult = "";
+    var visual_img = document.querySelector(".visual_img");
+    var image_count = 0;
 
-}
+    for (var i = 0; i < json.items.length; i++) {
+      promotionItemResult += promotionItemHTML.replace("{src}", json.items[i].productImageUrl);
+    };
+    visual_img.innerHTML = promotionItemResult;
 
-//draws promotion images
-function addThImages(oReq){
-	var json = JSON.parse(oReq.responseText);
-	var promotionItemHTML = document.querySelector("#promotionImages").innerHTML;
-	var promotionItemResult = "";
-	var visual_img = document.querySelector(".visual_img");
-	var image_count = 0;
+    visual_img.querySelectorAll("li").forEach(function() {
+      image_count++;
+    });
+    visual_img.style.width = (visual_img.offsetWidth * image_count) + "px";
+    promotion_image_move(image_count);
 
-	//get product image url from json and add to src path
-	//add promotionImages templates under '.visual_img'
-	for(var i = 0; i < json.items.length; i++){
-		promotionItemResult += promotionItemHTML.replace("{src}", json.items[i].productImageUrl);
-	};
-	visual_img.innerHTML = promotionItemResult;
-	
-	//count all the li tags to expand the promotion image area
-	visual_img.querySelectorAll("li").forEach(function() {
-		image_count++;
-	});
-	visual_img.style.width = (visual_img.offsetWidth * image_count) + "px";
-	
-	//show images one by one
-	promotion_image_move(image_count);
+  };
 
-};
+  function sendAjax(url) {
+    var oReq = new XMLHttpRequest;
+    oReq.open('GET', url);
+    oReq.setRequestHeader("Content-type", "application/json");
+    oReq.responseType = "text";
+    oReq.addEventListener('load', function() {
+      addThImages(this);
+    });
+    oReq.send();
+  };
 
-//get responseData and send it to parameter of function 'addThImages'
-function sendAjax(url){
-	var oReq = new XMLHttpRequest;
-	oReq.open('GET', url);
-	oReq.setRequestHeader("Content-type", "application/json");
-	oReq.responseType = "text";
-	oReq.addEventListener('load', function(){
-		addThImages(this);
-	});
-	oReq.send();
-};
-
-//sends ajax
-sendAjax("/reservationManagement/api/promotions");
+  sendAjax("/reservationManagement/api/promotions");
+});
