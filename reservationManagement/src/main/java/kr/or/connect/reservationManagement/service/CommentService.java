0e72f4f0275c75651bc43202da229171af0b6c23
ChoiKanghun <rkdgns4562@naver.com>
2020-06-12 13:04:39 +0900

    Create: created comment service & impl

---
+++ reservationManagement/src/main/java/kr/or/connect/reservationManagement/service/CommentService.java
@@ -0,0 +1,9 @@
+package kr.or.connect.reservationManagement.service;
+
+import java.util.List;
+
+import kr.or.connect.reservationManagement.dto.Comments;
+
+public interface CommentService {
+	float setCommentImages(List<Comments> comments);
+}
---
+++ reservationManagement/src/main/java/kr/or/connect/reservationManagement/service/impl/CommentServiceImpl.java
@@ -0,0 +1,33 @@
+package kr.or.connect.reservationManagement.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.or.connect.reservationManagement.dao.CommentImagesDao;
+import kr.or.connect.reservationManagement.dto.Comments;
+import kr.or.connect.reservationManagement.service.CommentService;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+
+	@Autowired
+	CommentImagesDao commentImagesDao;
+	
+	public float setCommentImages(List<Comments> comments) {
+		float averageScore = 0;
+		float division = 0;
+		
+		for(int i = 0; i < comments.size(); i++) {
+			comments.get(i).setCommentImages(commentImagesDao.getCommentImages(comments.get(i).getCommentId()));
+			if (comments.get(i).getScore() != null) {
+				averageScore += comments.get(i).getScore();
+				division += 1;
+			}
+		}
+		if (division != 0)
+			averageScore = averageScore / division;
+		return averageScore;
+	}
+}
