e803174ccd59d4a963b0820ca53025db63d467a7
ChoiKanghun <rkdgns4562@naver.com>
2020-06-17 07:55:18 +0900

    Add: bean for logging

--- reservationManagement/src/main/java/kr/or/connect/reservationManagement/config/WebMvcContextConfiguration.java
+++ reservationManagement/src/main/java/kr/or/connect/reservationManagement/config/WebMvcContextConfiguration.java
@@ -6,10 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import kr.or.connect.reservationManagement.interceptor.LogInterceptor;
 
 
 @Configuration
@@ -51,7 +54,11 @@ public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
 	public MultipartResolver multipartResolver() {
 		org.springframework.web.multipart.commons.CommonsMultipartResolver multipartResolver 
 	        = new org.springframework.web.multipart.commons.CommonsMultipartResolver();
-		//multipartResolver.setMaxUploadSize(10485760); // 1024 * 1024 * 10
 		return multipartResolver;
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LogInterceptor());
 	}
 }
