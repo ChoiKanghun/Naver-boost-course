e781e161d2847b34bc0301ce049adf585352ea1c
ChoiKanghun <rkdgns4562@naver.com>
2020-06-24 00:25:17 +0900

    Adapted Feedback

--- reservationManagement/src/main/java/kr/or/connect/reservationManagement/service/impl/FileServiceImpl.java
+++ reservationManagement/src/main/java/kr/or/connect/reservationManagement/service/impl/FileServiceImpl.java
@@ -1,27 +1,38 @@
 package kr.or.connect.reservationManagement.service.impl;
 
-import java.util.Calendar;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import kr.or.connect.reservationManagement.dto.EnrollImageFile;
+import kr.or.connect.reservationManagement.service.CommentService;
 import kr.or.connect.reservationManagement.service.FileService;
 
 @Service
 public class FileServiceImpl implements FileService {
 
+	@Autowired
+	CommentService commentService;
+	
 	@Override
-	public String genereateSaveFileName(String fileExtName) {
-		String saveFileName = "";
-		Calendar calendar = Calendar.getInstance();
+	public int downloadImage(MultipartFile file) throws IOException {
+		final String SAVE_PATH = "c:/tmp/";
+		String fileName = file.getOriginalFilename();
+		String fileExtName = fileName.substring(fileName.lastIndexOf("."), fileName.length());
+		String saveFileName = FileService.genereateSaveFileName(fileExtName);
+		String contentType = file.getContentType();
 		
-		saveFileName += calendar.get(Calendar.YEAR);
-		saveFileName += calendar.get(Calendar.MONTH);
-		saveFileName += calendar.get(Calendar.DATE);
-		saveFileName += calendar.get(Calendar.HOUR);
-		saveFileName += calendar.get(Calendar.MINUTE);
-		saveFileName += calendar.get(Calendar.SECOND);
-		saveFileName += calendar.get(Calendar.MILLISECOND);
-		saveFileName += fileExtName;
-		return saveFileName;
+		byte[] data = file.getBytes();
+		FileOutputStream fos = new FileOutputStream(SAVE_PATH + saveFileName);
+		fos.write(data);
+		fos.close();
+		EnrollImageFile enrollImageFile = new EnrollImageFile();
+		enrollImageFile.setSaveFileName(saveFileName);
+		enrollImageFile.setContentType(contentType);
+		enrollImageFile.setFileName(fileName);
+		return commentService.enrollImageFile(enrollImageFile);
 	}
 }
