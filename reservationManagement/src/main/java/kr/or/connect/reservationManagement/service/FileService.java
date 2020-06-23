f8336027097962719d8181689e595583b6a19c33
ChoiKanghun <rkdgns4562@naver.com>
2020-06-24 00:27:39 +0900

    Add: added downloadImage service

--- reservationManagement/src/main/java/kr/or/connect/reservationManagement/service/FileService.java
+++ reservationManagement/src/main/java/kr/or/connect/reservationManagement/service/FileService.java
@@ -1,5 +1,24 @@
 package kr.or.connect.reservationManagement.service;
 
+import java.io.IOException;
+import java.util.Calendar;
+
+import org.springframework.web.multipart.MultipartFile;
+
 public interface FileService {
-	String genereateSaveFileName(String fileExtName);
+	public static String genereateSaveFileName(String fileExtName) {
+		String saveFileName = "";
+		Calendar calendar = Calendar.getInstance();
+		
+		saveFileName += calendar.get(Calendar.YEAR);
+		saveFileName += calendar.get(Calendar.MONTH);
+		saveFileName += calendar.get(Calendar.DATE);
+		saveFileName += calendar.get(Calendar.HOUR);
+		saveFileName += calendar.get(Calendar.MINUTE);
+		saveFileName += calendar.get(Calendar.SECOND);
+		saveFileName += calendar.get(Calendar.MILLISECOND);
+		saveFileName += fileExtName;
+		return saveFileName;
+	};
+	int downloadImage(MultipartFile file) throws IOException;
 }
