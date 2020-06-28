771779792caf15ad27eb06659fedd332b949a8ea
ChoiKanghun <rkdgns4562@naver.com>
2020-06-28 17:24:37 +0900

    Create: file controller for getting images under C:/

---
+++ reservationManagement/src/main/java/kr/or/connect/reservationManagement/controller/FileController.java
@@ -0,0 +1,25 @@
+package kr.or.connect.reservationManagement.controller;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/file")
+public class FileController {
+	@GetMapping(path="")
+	public void setImageBySaveFileName(@RequestParam String saveFileName,
+			HttpServletResponse response) throws IOException {
+		StringBuilder sb = new StringBuilder("file:///c:/tmp/");
+		sb.append(saveFileName);
+        URL fileUrl = new URL(sb.toString());
+        IOUtils.copy(fileUrl.openStream(), response.getOutputStream());
+	}
+}
