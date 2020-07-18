e8fe9e4e1d641ae73b8691ce9efdb99f778b425a
ChoiKanghun <rkdgns4562@naver.com>
2020-07-18 23:45:01 +0900

    Feat: it gets id, returns List of DTO

--- reservationManagement/src/main/java/kr/or/connect/reservationManagement/dao/GetSaveFileNameByReservationUserCommentImageIdDao.java
+++ reservationManagement/src/main/java/kr/or/connect/reservationManagement/dao/GetSaveFileNameByReservationUserCommentImageIdDao.java
@@ -1,15 +1,35 @@
 package kr.or.connect.reservationManagement.dao;
 
+import static kr.or.connect.reservationManagement.dao.Sqls.GET_SAVEFILENAME_BY_RESERVATIONUSERINFOID;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import kr.or.connect.reservationManagement.dto.Items;
+import kr.or.connect.reservationManagement.dto.GetSaveFileNameByReservationUserCommentImageId;
+
 
 @Repository
 public class GetSaveFileNameByReservationUserCommentImageIdDao {
 	private NamedParameterJdbcTemplate jdbc;
-	private RowMapper<Items> rowMapper
-	= BeanPropertyRowMapper.newInstance(Items.class);
+	private RowMapper<GetSaveFileNameByReservationUserCommentImageId> rowMapper
+	= BeanPropertyRowMapper.newInstance(GetSaveFileNameByReservationUserCommentImageId.class);
+	
+	public GetSaveFileNameByReservationUserCommentImageIdDao(DataSource dataSource) {
+		jdbc = new NamedParameterJdbcTemplate(dataSource);
+	}
+	
+	public List<GetSaveFileNameByReservationUserCommentImageId> getSaveFileNameByReservationUserCommentImageId(int reservationUserCommentImageId) {
+		Map<String, Integer> params = new HashMap<>();
+		params.put("reservation_user_comment_image_id", reservationUserCommentImageId);
+		
+		return jdbc.query(GET_SAVEFILENAME_BY_RESERVATIONUSERINFOID, params, rowMapper);
+	}
 }
---
+++ reservationManagement/src/main/java/kr/or/connect/reservationManagement/dto/GetSaveFileNameByReservationUserCommentImageId.java
@@ -0,0 +1,19 @@
+package kr.or.connect.reservationManagement.dto;
+
+public class GetSaveFileNameByReservationUserCommentImageId {
+	private String saveFileName;
+
+	public String getSaveFileName() {
+		return saveFileName;
+	}
+
+	public void setSaveFileName(String saveFileName) {
+		this.saveFileName = saveFileName;
+	}
+
+	@Override
+	public String toString() {
+		return "GetSaveFileNameByReservationUserCommentImageId [saveFileName=" + saveFileName + "]";
+	}
+}
+
