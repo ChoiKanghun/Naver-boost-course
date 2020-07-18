da0bab477c1388dedaf62db825bcb19ab1dc84e7
ChoiKanghun <rkdgns4562@naver.com>
2020-07-18 23:59:19 +0900

    Add: getSaveFileNameByReservationUserCommentImageId

--- reservationManagement/src/main/java/kr/or/connect/reservationManagement/service/FileService.java
+++ reservationManagement/src/main/java/kr/or/connect/reservationManagement/service/FileService.java
@@ -7,4 +7,5 @@ import org.springframework.web.multipart.MultipartFile;
 public interface FileService {
 	public String genereateSaveFileName(String fileExtName);
 	int saveImage(MultipartFile file) throws IOException;
+	String getSaveFileNameByReservationUserCommentImageId(int reservationUserCommentImageId);
 }
