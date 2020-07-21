6fe88e6eb9f7b601c35ffd174e8abf0475bea6e0
ChoiKanghun <rkdgns4562@naver.com>
2020-07-21 13:40:15 +0900

    Style: beautified

--- reservationManagement/src/main/java/kr/or/connect/reservationManagement/controller/FileController.java
+++ reservationManagement/src/main/java/kr/or/connect/reservationManagement/controller/FileController.java
@@ -6,17 +6,24 @@ import java.net.URL;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.or.connect.reservationManagement.service.FileService;
+
 @RestController
 @RequestMapping("/api/file")
 public class FileController {
+	@Autowired
+	private FileService fileService;
+	
 	@GetMapping
-	public void downloadImageBySaveFileName(@RequestParam String saveFileName,
+	public void downloadImageBySaveFileName(@RequestParam int imageId,
 			HttpServletResponse response) throws IOException {
+		String saveFileName = fileService.getSaveFileNameByReservationUserCommentImageId(imageId);
 		StringBuilder sb = new StringBuilder("file:///c:/tmp/");
 		sb.append(saveFileName);
         URL fileUrl = new URL(sb.toString());
